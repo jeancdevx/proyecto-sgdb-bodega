da588173e271a31a72a5aec1a9fdade365a7fac1
jeancdev <jcode2006@gmail.com>
2025-04-27 16:39:29 -0500

    feat: :sparkles: add SQL script to drop existing tables and database for cleanup

---
+++ database/delete/tablas.sql
@@ -0,0 +1,10 @@
+DROP TABLE boleta;
+DROP TABLE categoria;
+DROP TABLE cliente;
+DROP TABLE detalle_venta;
+DROP TABLE empleado;
+DROP TABLE factura;
+DROP TABLE producto;
+DROP TABLE venta;
+
+DROP DATABASE db_ventas;
