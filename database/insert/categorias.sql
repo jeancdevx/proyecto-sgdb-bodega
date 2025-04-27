3ee6a01f461d14b784a4e2d92d367c7bcf5702f1
jeancdev <jcode2006@gmail.com>
2025-04-27 16:41:09 -0500

    feat: :sparkles: add SQL script to insert categories into the database

---
+++ database/insert/categorias.sql
@@ -0,0 +1,9 @@
+INSERT INTO categoria (nombre) VALUES
+('Snacks'),
+('Bebidas Gaseosas'),
+('Artículos de Librería'),
+('Lácteos'),
+('Panadería'),
+('Abarrotes'),
+('Productos de Limpieza'),
+('Cuidado Personal');
