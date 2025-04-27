294222cc2136fd82594d836eef6422903fb57ce3
jeancdev <jcode2006@gmail.com>
2025-04-27 16:41:15 -0500

    feat: :sparkles: add SQL script to insert products into the database

---
+++ database/insert/productos.sql
@@ -0,0 +1,56 @@
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+-- Snacks
+('Papas Fritas', 1.50, 100, 1),
+('Chocolates', 2.00, 50, 1),
+('Galletas', 1.20, 80, 1),
+('Chicles', 0.50, 200, 1),
+('Barritas Energéticas', 1.80, 60, 1);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Coca-Cola', 2.50, 100, 2),
+('Pepsi', 2.40, 80, 2),
+('Fanta', 2.30, 70, 2),
+('Sprite', 2.20, 90, 2),
+('Inca Kola', 2.60, 60, 2);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Cuadernos', 3.50, 40, 3),
+('Lápices', 0.80, 150, 3),
+('Borradores', 0.50, 100, 3),
+('Reglas', 1.20, 60, 3),
+('Marcadores', 2.00, 50, 3);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Leche Entera', 3.00, 50, 4),
+('Yogurt', 2.50, 40, 4),
+('Queso', 4.00, 30, 4),
+('Mantequilla', 3.50, 20, 4),
+('Crema de Leche', 2.80, 25, 4);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Pan Francés', 0.30, 300, 5),
+('Pan Integral', 0.50, 200, 5),
+('Croissants', 1.20, 100, 5),
+('Donas', 1.50, 80, 5),
+('Empanadas', 2.00, 60, 5);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Arroz', 2.50, 100, 6),
+('Fideos', 1.80, 80, 6),
+('Aceite', 5.00, 50, 6),
+('Azúcar', 2.20, 90, 6),
+('Sal', 1.00, 120, 6);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Detergente', 5.50, 40, 7),
+('Jabón Líquido', 3.80, 50, 7),
+('Esponjas', 1.20, 100, 7),
+('Cloro', 2.50, 60, 7),
+('Desinfectante', 4.00, 30, 7);
+
+INSERT INTO producto (nombre, precio, stock, idCategoria) VALUES
+('Shampoo', 6.00, 30, 8),
+('Jabón de Tocador', 1.50, 100, 8),
+('Crema Dental', 2.50, 80, 8),
+('Desodorante', 4.00, 50, 8),
+('Toallas Húmedas', 3.00, 40, 8);
